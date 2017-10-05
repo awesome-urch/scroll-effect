66a8904eddcb36e65c047812c82a27d22f523ddf
Nnabugwu Michael U <32470486+awesome-urch@users.noreply.github.com>
2017-10-05 22:21:38 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,14 +1,3 @@
-$.fn.extend(
-{
-	closeModalBox: function()
-	{
-		var id = $(this).attr('id');
-		var close_icon = $('<i>').css({'position':'fixed','top':'0','right':'0','z-index':'10','cursor':'pointer'}).addClass('material-icons box-shade circle').html('cancel').appendTo($(this)).click(function(){
-		  	$('#'+id).modal('close');
-			//$('#c'+p_id+'>li>.collapsible-body>ul')
-		  });
-	}
-})
 
 $.fn.extend({
 	scrollEffect: function()
@@ -21,38 +10,21 @@ $.fn.extend({
 			
 			$(div).each(function(index, element) 
 			{
-				//console.log($(this))
 
 				if($(window).scrollTop() >  $(this).offset().top - dh1)
 				{
-					console.log($(this))
-					$(this).delay(800).addClass("reset_style1");
+
+					$(this).delay(800).addClass("reset-style");
 				}
 				
 				
 			});
 			
-			
-			/*if($(window).scrollTop() >  $(this).offset().top - dh1)
-			{
-				area.delay(800).addClass("reset_style1");
-			}*/
 		});
 		
 	}
 	
 })
-
-$.fn.extend({
-	questionBox:function(q,f,h)
-	{		
-		$('#_question_action').off('click');
-		$('#_question_action').click(f);
-		$('#_question_text').html(q)
-
-		$('#_question_dialog').modal('open');
-	}
-});
 
 
 
